11e02bd20b17602d1f4b2fe579d932d353d00222
nithi-sree <60870413+nithi-sree@users.noreply.github.com>
2020-02-14 00:43:19 -0800

    Add files via upload

---
+++ Conversion.py
@@ -0,0 +1,5 @@
+dec = int(input("Enter the decimal number: "))
+
+print("Binary value of ",dec,"is ",bin(dec))
+print("Octal value ",dec,"is ",oct(dec))
+print("Hexadecimal Value ",dec,"is ",hex(dec))
