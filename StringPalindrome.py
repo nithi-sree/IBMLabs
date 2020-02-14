d50d8f20d388b00aac817e650be3e43b5242297d
nithi-sree <60870413+nithi-sree@users.noreply.github.com>
2020-02-14 01:31:45 -0800

    Lab 2

---
+++ StringPalindrome.py
@@ -0,0 +1,10 @@
+my_str = input("Enter the string: ")
+
+inpStr = my_str.casefold()
+
+rev_str = reversed(inpStr)
+
+if list(inpStr) == list(rev_str):
+    print(f"{my_str} is a Palindrome.")
+else:
+    print(f"{my_str} is a Palindrome.")
