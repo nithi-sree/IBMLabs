29ac26b03cd62c8aaceff0293499de941c671e9f
nithi-sree <60870413+nithi-sree@users.noreply.github.com>
2020-02-14 00:56:13 -0800

    Lab 2

--- Conversion.py
+++ Conversion.py
@@ -1,5 +1,4 @@
 dec = int(input("Enter the decimal number: "))
-
 print("Binary value of ",dec,"is ",bin(dec))
 print("Octal value ",dec,"is ",oct(dec))
 print("Hexadecimal Value ",dec,"is ",hex(dec))
